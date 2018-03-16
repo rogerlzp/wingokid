8c24fac0d54bd798f77eb9d9c9baec56a9809ad8
roger <rogerlzp@gmail.com>
2018-03-16 18:05:25 +0800

    修改页面

---
+++ js/wingokid.js
@@ -0,0 +1,12 @@
+function resizeElementHeight(element) {
+    var height = 0;
+    var body = window.document.body;
+    if (window.innerHeight) {
+        height = window.innerHeight;
+    } else if (body.parentElement.clientHeight) {
+        height = body.parentElement.clientHeight;
+    } else if (body && body.clientHeight) {
+        height = body.clientHeight;
+    }
+    element.style.height = ((height - element.offsetTop) + "px");
+}
