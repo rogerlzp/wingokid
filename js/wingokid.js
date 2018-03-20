0003f50f3c229ca3446388cc9e33c26b648fd8c7
roger <rogerlzp@gmail.com>
2018-03-20 20:40:20 +0800

    页面调整

--- js/wingokid.js
+++ js/wingokid.js
@@ -130,21 +130,37 @@ function c() {
         n.eq(r).click(), r >= n.length - 1 ? r = 0 : r++
     }
 
-    var t, n = $(".pagination span"), i = $(".slide"), r = 1, c = !1, d = [1, 0, 2, 3, 4];
+    var t, n = $(".pagination span"), i = $(".slide"), r = 1, c = !1, d = [1, 0, 2];
     n.click(function () {
         var o = $(this).hasClass("active");
-        n.removeClass("active"), $(this).addClass("active"), o || $(this).index() != d[1] || (e(d[1] + 1),
+        n.removeClass("active"), $(this).addClass("active"),
+        o || $(this).index() != d[1] ||
+        (e(d[1] + 1),
             $.each(i, function (e, o) {
                 $(o).stop().fadeOut("fast")
-            }), s(), l()), o || $(this).index() != d[0] || (e(d[0] + 1), $.each(i, function (e, o) {
+            }), s(), l()),
+        o || $(this).index() != d[0] ||
+        (e(d[0] + 1), $.each(i, function (e, o) {
             $(o).hide()
-        }), $(".slide4").stop().fadeIn("normal"), a()), o || $(this).index() != d[2] || (e(d[2] + 1), $.each(i, function (e, o) {
+        }),
+            $(".slide7").stop().fadeIn("normal"), s()),
+        // o || $(this).index() != d[2] ||
+        // (e(d[2] + 1), $.each(i, function (e, o) {
+        //     $(o).hide()
+        // }),
+        //     $(".slide0").stop().fadeIn("normal"), s()),
+        // o || $(this).index() != d[3] ||
+        // (e(d[3] + 1), $.each(i, function (e, o) {
+        //     $(o).hide()
+        // }),
+        //     $(".slide5").stop().fadeIn("normal"), s()),
+        o || $(this).index() != d[2] ||
+        (e(d[2] + 1), $.each(i, function (e, o) {
             $(o).hide()
-        }), $(".slide0").stop().fadeIn("normal"), s()), o || $(this).index() != d[3] || (e(d[3] + 1), $.each(i, function (e, o) {
-            $(o).hide()
-        }), $(".slide5").stop().fadeIn("normal"), s()), o || $(this).index() != d[4] || (e(d[4] + 1), $.each(i, function (e, o) {
-            $(o).hide()
-        }), $(".slide3").stop().fadeIn("normal"), a()), $(this).index() != d[0] && $(".js-mouse-gif-container").show()
+        }),
+            $(".slide3").stop().fadeIn("normal"),
+            a()),
+        $(this).index() != d[0] && $(".js-mouse-gif-container").show()
     }), t = setInterval(o, 6e3), $(window).scroll(function () {
         $(window).scrollTop() < $(window).height() && c ? (t = setInterval(o, 6e3), c = !1) : $(window).scrollTop() > $(window).height() && (clearInterval(t), c = !0)
     }), $(".js-hover-btn").mousemove(function () {
@@ -154,12 +170,13 @@ function c() {
     })
 }
 function l() {
-    $(".slide6").css("opacity", "0").show(),
-        $(".slide6_text_wrapper img").css("opacity", "0"),
-        $(".liutao_single_pic").css("opacity", "0"),
-        $(".slide6").stop(!0, !1).animate({opacity: "1"}, 300, "linear"), setTimeout(function () {
-        d($(".text1"), 500), d($(".liutao_single_pic"), 500)
-    }, 300)
+    $(".slide6").css("opacity", "1").show()
+    // $(".slide6").css("opacity", "0").show(),
+    //     $(".slide6_text_wrapper img").css("opacity", "0"),
+    //     $(".liutao_single_pic").css("opacity", "0"),
+    //     $(".slide6").stop(!0, !1).animate({opacity: "1"}, 300, "linear"), setTimeout(function () {
+    //     d($(".text1"), 500), d($(".liutao_single_pic"), 500)
+    // }, 300)
 }
 
 function d(e, o, t) {
